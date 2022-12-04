0829000a6ebb5fb85627b9d65427199ef5020847
Mike Babs <mikeybabs40@gmail.com>
2022-12-04 23:45:48 +0000

    Added ec2 resources, security group and VPC

--- main.tf
+++ main.tf
@@ -7,10 +7,11 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "terraform-dev-bucket-a"
+    bucket         = "terraform-dev-b"
     key            = "environment/dev/terraform.tfstate"
     dynamodb_table = "terraform-locks"
     region         = "eu-west-1"
+    encrypt        = true
   }
 }
 
@@ -19,28 +20,28 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-resource "aws_s3_bucket" "terraform-dev-bucket-a" {
-  bucket        = "terraform-dev-bucket-a"
+resource "aws_s3_bucket" "terraform-dev-b" {
+  bucket        = "terraform-dev-b"
   force_destroy = "true"
 }
 
 
 #Bucket Versioning 
-resource "aws_s3_bucket_acl" "terraform-dev-bucket-a-acl" {
-  bucket = aws_s3_bucket.terraform-dev-bucket-a.id
+resource "aws_s3_bucket_acl" "terraform-dev-b-acl" {
+  bucket = aws_s3_bucket.terraform-dev-b.id
   acl    = "private"
 }
 
-resource "aws_s3_bucket_versioning" "terraform-dev-bucket-a-versioning" {
-  bucket = aws_s3_bucket.terraform-dev-bucket-a.id
+resource "aws_s3_bucket_versioning" "terraform-dev-b-versioning" {
+  bucket = aws_s3_bucket.terraform-dev-b.id
   versioning_configuration {
     status = "Enabled"
   }
 }
 
 #Bucket Encryption 
-resource "aws_s3_bucket_server_side_encryption_configuration" "terraform-dev-bucket-a-encryption" {
-  bucket = aws_s3_bucket.terraform-dev-bucket-a.bucket
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform-dev-b-encryption" {
+  bucket = aws_s3_bucket.terraform-dev-b.bucket
 
   rule {
     apply_server_side_encryption_by_default {
@@ -51,7 +52,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform-dev-buc
 
 #Dynamodb 
 resource "aws_dynamodb_table" "terraform-locks" {
-  name         = "value"
+  name         = "terraform-locks"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
   attribute {
@@ -61,3 +62,53 @@ resource "aws_dynamodb_table" "terraform-locks" {
 
 }
 
+
+resource "aws_instance" "instance_1" {
+  ami             = "ami-01cae1550c0adea9c" # eu-west-1
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instances_sg.name]
+  user_data       = <<-EOF
+              #!/bin/bash
+              echo "Hello, World 1" > index.html
+              python3 -m http.server 8080 &
+              EOF
+
+}
+
+resource "aws_instance" "instance_2" {
+  ami             = "ami-01cae1550c0adea9c" # eu-west-1
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instances_sg.name]
+  user_data       = <<-EOF
+              #!/bin/bash
+              echo "Hello, World 1" > index.html
+              python3 -m http.server 8080 &
+              EOF
+
+}
+
+
+
+data "aws_vpc" "default_vpc" {
+  default = true
+}
+
+
+data "aws_subnet_ids" "default_subnet" {
+  vpc_id = data.aws_vpc.default_vpc.id
+
+}
+
+resource "aws_security_group" "instances_sg" {
+  name = "instances-sg"
+
+}
+
+resource "aws_security_group_rule" "allow_http_inbound" {
+  type              = "ingress"
+  from_port         = 8080
+  to_port           = 8080
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.instances_sg.id
+}
